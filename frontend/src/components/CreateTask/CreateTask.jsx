c79cdaabe27247d20ea227bde8d64a02d9b36a9c
Manuel <nacermanuel@gmail.com>
2023-04-17 11:36:10 -0500

    Maquetacion vista externa Create Task

--- frontend/src/components/CreateTask/CreateTask.jsx
+++ frontend/src/components/CreateTask/CreateTask.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './CreateTask.css'
+import icon1 from '../../../public/mas.png'
 
 export default function CreateTask() {
   return (
     <div className='CreateTask'>
-        CreateTask
+        <input type="text" />
+        <img src={icon1} alt="icono crear" onClick={()=> alert('Crear')}/>
     </div>
   )
 }
