73ff316338dca92ac3f6008da309431519b6e502
Manuel <nacermanuel@gmail.com>
2023-04-26 20:10:51 -0500

    Modal View Task first edition

--- frontend/src/components/ModalTask.jsx
+++ frontend/src/components/ModalTask.jsx
@@ -2,6 +2,10 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
 import * as React from 'react';
 
 const style = {
@@ -12,17 +16,18 @@ const style = {
   height: '80vh',
   width: '55vw',
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  border: '2px solid #39e3e3',
   boxShadow: 24,
   p: 4,
   display: 'flex',
   flexDirection: 'column',
-  justifyContent: 'center',
+  justifyContent: 'space-around',
   alignItems: 'center',
   gap: 2
 };
 
 export default function ModalTask({open , onClose, data}) {
+
   return (
         <Modal
           open={open}
@@ -31,15 +36,20 @@ export default function ModalTask({open , onClose, data}) {
           aria-describedby="modal-modal-description"
         >
           <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-
-            </Typography>
             <TextField id="outlined-basic" label="Título" variant="outlined" defaultValue={data.name} sx={{width:"100%"}}/>
             <TextField id="outlined-basic" label="Descripción" variant="outlined" defaultValue={data.description} multiline rows={4} sx={{width:"100%"}}/>
-
+            <Chip label={`Esta tarea fue creada el ${data.name}`} color="primary" sx={{fontSize: '0.9rem', width:'100%'}}/>
             <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              Aqui voy a poner una frase motivaiconal.
             </Typography>
+
+            {/* Aqui podria venir el boton en disable y si hay cambios se activa */}
+              <Box sx={{display: 'flex', flexDirection: 'row', width: '55vw', justifyContent: 'center', gap: '5vw'}}>
+                <Button variant="contained">Actualizar</Button>
+                <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>Eliminar</Button>
+                <Button variant="contained" endIcon={<SendIcon />} color="secondary">Hacer Ahora</Button>
+              </Box>
+
           </Box>
         </Modal>
   )
