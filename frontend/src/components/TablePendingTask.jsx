9e3c717c5f1538fa0ee43819e39eba8d7b40c461
Manuel <nacermanuel@gmail.com>
2023-04-27 20:03:31 -0500

    Listado de pendientes alimentado desde el redux

--- frontend/src/components/TablePendingTask.jsx
+++ frontend/src/components/TablePendingTask.jsx
@@ -1,21 +1,23 @@
 import Stack from '@mui/joy/Stack';
 import React from 'react'
+import { useSelector } from 'react-redux';
 
-
-import { mockTasks } from '../mocks/mockTask';
 import InputCreateTask from './InputCreateTask';
 import TaskCard from './TaskCard';
 
 export default function TablePendingTask() {
+  const reduxState = useSelector((state) => state.tareas)
   return (
-
     <div className='bg-slate-300 h-[77vh] w-[55vh] p-3 rounded-md shadow-[0_0_10px_4px_rgba(0,0,0)]'>
       <div className=' h-[63vh] overflow-y-auto'>
+        { reduxState.loading && <div>Loading</div> }
+        { !reduxState.loading && reduxState.error ? <div>Error: {reduxState.error}</div> : null}
+        { !reduxState.loading && reduxState.tareas.length ?
         <Stack spacing={1} sx={{ alignItems:'center', paddingTop:'5px' }} > {/*https://mui.com/material-ui/react-stack/ */}
-          {mockTasks.map(e => {
+          {reduxState.tareas.map(e => {
             return <TaskCard data={e}/>
           })}
-        </Stack>
+        </Stack> : null }
       </div>
       <div>
         <InputCreateTask/>
