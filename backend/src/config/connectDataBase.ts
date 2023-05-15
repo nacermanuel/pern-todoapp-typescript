e3548e647b0f50860bff5f03efe70f2da3c55ee7
Manuel <nacermanuel@gmail.com>
2023-05-15 10:18:24 -0500

    Set DBURI via enviroment variables

--- backend/src/config/connectDataBase.ts
+++ backend/src/config/connectDataBase.ts
@@ -1,16 +1,27 @@
-import { Sequelize } from "sequelize-typescript";
+import { Sequelize, SequelizeOptions } from "sequelize-typescript";
+import dotenv from 'dotenv'
+dotenv.config()
+
 import { SequelizeUserModel } from "../contexts/todoApp/User/infrastructure/persistence/sequelize/SequelizeUserModel";
 import { SequelizeTaskModel } from "../contexts/todoApp/Task/infrastructure/persistence/sequelize/SequelizeTaskModel";
 import { SequelizeListTaskModel } from "../contexts/todoApp/ListTask/infrastructure/persistence/sequelize/SequelizeListTaskModel";
 
 //INICIALIZAR UNA INSTANCIA DE SEQUILIZE CON LOS DATOS DE LA BBDD
-const db_sequelize = new Sequelize({
-    database: 'todoapppern',
-    dialect: 'postgres',
-    username: 'postgres',
-    password: '1234',
-    storage: ':memory:',
-})
+// const db_sequelize = new Sequelize({
+//     database: 'todoapppern',
+//     dialect: 'postgres',
+//     username: 'postgres',
+//     password: '1234',
+//     storage: ':memory:',
+// })
+
+const db_uri: string | undefined = process.env.DB_URI;
+
+if (!db_uri) {
+    throw new Error('DB_URI is not defined.');
+}
+
+const db_sequelize = new Sequelize(db_uri) // Example for postgres
 
 //PENDIENTE DE PASAR LOS DATOS MEDIANTE LAS VARIABLES DE ENTORNO PARA DAR MAYOR SEGURIDAD 
 //Y CORRECTAS PRACTICAS
